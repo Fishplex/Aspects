1a630355c9f75052a8ac5e2c185a63a4d6385ef7
missingbinary <92999349+missingbinary@users.noreply.github.com>
2021-12-11 13:27:36 -0400

    Initial commit

---
+++ src/main/java/net/fabricmc/fishplex/mixin/AspectsMixin.java
@@ -0,0 +1,16 @@
+package net.fabricmc.fishplex.mixin;
+
+import net.fabricmc.fishplex.Aspects;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class AspectsMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		Aspects.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
